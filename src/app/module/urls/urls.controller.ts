2ad4d807a1db05d4f337f49388c0be94d2c86a41
smmunna <minhazulabedinmunna@gmail.com>
2025-02-01 19:49:38 +0600

    updated

--- src/app/module/urls/urls.controller.ts
+++ src/app/module/urls/urls.controller.ts
@@ -1,11 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 
 
-const createUrls = async (req: Request, res: Response, next: NextFunction) =>{
+
+const createUrls = (req: Request, res: Response, next: NextFunction) => {
+    try {
         const { url } = req.body;
 
-}
+        const shortCode = Math.random().toString(36).substring(2, 8);;
+        const shortenerUrl =`${req.protocol}://${req.host}/${shortCode}`;
+
+        res.json({
+            message: `URL "${url}" has been created successfully`,
+            shortenedUrl: shortenerUrl,
+            shortCode: shortCode,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
 
 export const urlsController = {
-    createUrls
-}
+    createUrls,
+};
